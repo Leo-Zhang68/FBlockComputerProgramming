5a480a5c1509101bf1d866cb120895a567604a82
Leo-Zhang68 <Leo.Zhang@eaglebrook.org>
2022-01-24 14:01:33 -0500

    jan 24 class

---
+++ turtlename.py
@@ -0,0 +1,55 @@
+from curses import BUTTON1_PRESSED
+from turtle import *
+
+color("orange", "yellow")
+
+penup()
+left(180)
+forward(500)
+
+begin_fill()
+pendown()
+pensize(5)
+right(90)
+forward(150)
+right(90)
+forward(30)
+right(90)
+forward(125)
+left(90)
+forward(75)
+right(90)
+forward(25)
+right(90)
+forward(105)
+end_fill()
+
+
+
+right(180)
+penup()
+forward(225)
+left(90)
+forward(75)
+right(90)
+begin_fill()
+pendown()
+forward(75)
+left(90)
+circle(37.5, 180, None)
+penup()
+forward(15)
+left(90)
+pendown()
+forward(100)
+left(90)
+circle(65, 270, None)
+left(90)
+forward(15)
+left(90)
+circle(-40, 105, None)
+
+
+
+
+done()
