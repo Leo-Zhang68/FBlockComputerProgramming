408f7a24ce01c7e79073e9bf05440e85717634a9
Leo-Zhang68 <Leo.Zhang@eaglebrook.org>
2022-02-02 13:55:58 -0500

    feb 2 class

--- moreturtles.py
+++ moreturtles.py
@@ -1,25 +1,43 @@
+from calendar import c
 from turtle import Turtle
 
 
 monkey = Turtle()
 frog = Turtle()
-monkey.pencolor("yellow")
-frog.pencolor("yellow")
-monkey.shape("turtle")
-frog.shape("turtle")
+
 
 
 s = monkey.getscreen()
+monkey.color("#ffe600", "orange")
+frog.color("#ffe600", "green")
+s.bgcolor("#f27cdb")
 
 
 
 for turtle in [monkey, frog]:
+    turtle.shape("turtle")
     turtle.left(45)
     turtle.pensize(10)
     turtle.penup()
     turtle.forward(200)
     turtle.right(180)
     turtle.pendown()
+
+def mcdonaldsM():
+    for chunkymonky in [monkey, frog]:
+        chunkymonky.penup()
+        chunkymonky.goto(-300,200)
+        chunkymonky.right(40)
+        chunkymonky.forward(100)
+        chunkymonky.pendown()
+    monkey.right(90)
+    frog.left(90)
+    frog.forward(50)
+    monkey.forward(50)
+    monkey.circle(-50, 180, None)
+    frog.circle(50, 180, None)
+    frog.hideturtle()
+    monkey.hideturtle()
 
 
 def mcdonaldsFryBecauseBananaIsHard():
@@ -30,10 +48,10 @@ def mcdonaldsFryBecauseBananaIsHard():
     frog.left(135)
     frog.circle(-375, 90, None)
 
-    frog.left(135)
-    frog.forward(75)
-    frog.back(75)
-    frog.left(90)
+    frog.right(135)
+    monkey.right(45)
+    monkey.forward(75)
+    monkey.left(45)
     frog.forward(50)
     frog.right(45)
     frog.circle(375, 40, None)
@@ -60,11 +78,11 @@ def mcdonaldsFryBecauseBananaIsHard():
 
 
 mcdonaldsFryBecauseBananaIsHard()
+mcdonaldsM()
 
-for bodacious in [monkey, frog]:
 
-    bodacious.penup()
-    bodacious.goto(0,400)
+
+    
 
 
 
