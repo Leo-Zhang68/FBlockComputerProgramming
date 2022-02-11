5ea6f023cef886ede990d2b05f5fc20ddf118222
Leo-Zhang68 <Leo.Zhang@eaglebrook.org>
2022-02-11 09:31:11 -0500

    feb 11 class

--- Evenmoreturtles.py
+++ Evenmoreturtles.py
@@ -1,43 +1,64 @@
-from turtle import Turtle
+from turtle import Turtle, onkeypress
+from xmlrpc.client import boolean
 
 mouse = Turtle()
+rectT = Turtle()
 mouse.penup()
 mouse.speed(1)
 mouse.shapesize(5)
 
+
+width, height = 250, 250
+
 s = mouse.getscreen()
 
 s.bgcolor("#d6be54")
-
-
+Upok = True
+distance = 10
 
 def mouseUp():
-        
-    mouse.speed(1000)
+   
+    mouse.speed(50)
     mouse.setheading(90)
     mouse.speed(1)
-    mouse.forward(1000)
 
 def mouseRight():
 
-    mouse.speed(1000)
+    mouse.speed(50)  
     mouse.setheading(0)
     mouse.speed(1)
-    mouse.forward(1000)
+    
 
 def mouseLeft():
 
     mouse.speed(1000)
     mouse.setheading(180)
     mouse.speed(1)
-    mouse.forward(1000)
+   
 
 def mouseDown():
 
     mouse.speed(1000)
     mouse.setheading(270)
     mouse.speed(1)
-    mouse.forward(1000)
+    
+
+
+rectT.speed(100)
+rectT.penup()
+rectT.forward(width)
+rectT.left(90) 
+rectT.pendown()
+rectT.forward(height)
+rectT.left(90)
+rectT.forward(width*2)
+rectT.left(90)
+rectT.forward(height*2)
+rectT.left(90)
+rectT.forward(width*2)
+rectT.left(90)
+rectT.forward(height)
+
 
 s.listen()
 s.onkeypress(mouseUp, "Up")
@@ -46,5 +67,13 @@ s.onkeypress(mouseLeft, "Left")
 s.onkeypress(mouseDown, "Down")
 
 
-s.mainloop()
+while True:
+    
+    if (mouse.ycor() < height and
+        mouse.ycor() > -height and
+        mouse.xcor() < width and
+        mouse.xcor() > -width):
+        mouse.forward(distance)
+
+    s.update()
 
