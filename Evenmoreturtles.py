6d5dbbf5c47a00b54998610942c16359136f756a
Leo-Zhang68 <Leo.Zhang@eaglebrook.org>
2022-02-14 11:10:27 -0500

    feb 14 class

--- Evenmoreturtles.py
+++ Evenmoreturtles.py
@@ -1,16 +1,31 @@
-from turtle import Turtle, onkeypress
-from xmlrpc.client import boolean
+
+from turtle import Turtle, setup
+width, height = 500, 400
+
+setup(1400, 1000)
 
 mouse = Turtle()
+cheeseDrawMan = Turtle()
 rectT = Turtle()
 mouse.penup()
 mouse.speed(1)
-mouse.shapesize(5)
+mouse.shapesize(3)
 
 
-width, height = 250, 250
+def drawCheese():
+    cheeseDrawMan.hideturtle()
+
+
+def cheeseGeneration():
+    print("hey future leo do the code haha suffer")
+cheeseGeneration()
+
+
+
 
 s = mouse.getscreen()
+s.screensize(448, 252)
+s.title("kitchen")
 
 s.bgcolor("#d6be54")
 Upok = True
@@ -43,21 +58,25 @@ def mouseDown():
     mouse.speed(1)
     
 
+def makeBoundary():
+    rectT.speed(100)
+    rectT.penup()
+    rectT.forward(width)
+    rectT.left(90) 
+    rectT.pendown()
+    rectT.forward(height)
+    rectT.left(90)
+    rectT.forward(width*2)
+    rectT.left(90)
+    rectT.forward(height*2)
+    rectT.left(90)
+    rectT.forward(width*2)
+    rectT.left(90)
+    rectT.forward(height)
+    rectT.hideturtle()
+makeBoundary()
 
-rectT.speed(100)
-rectT.penup()
-rectT.forward(width)
-rectT.left(90) 
-rectT.pendown()
-rectT.forward(height)
-rectT.left(90)
-rectT.forward(width*2)
-rectT.left(90)
-rectT.forward(height*2)
-rectT.left(90)
-rectT.forward(width*2)
-rectT.left(90)
-rectT.forward(height)
+
 
 
 s.listen()
@@ -68,11 +87,21 @@ s.onkeypress(mouseDown, "Down")
 
 
 while True:
-    
-    if (mouse.ycor() < height and
-        mouse.ycor() > -height and
-        mouse.xcor() < width and
-        mouse.xcor() > -width):
+    allowedForward = True
+    # if (mouse.ycor() < height and
+    #     mouse.ycor() > -height and
+    #     mouse.xcor() < width and
+    #     mouse.xcor() > -width):
+    if mouse.ycor() >= height and mouse.heading() == 90:
+        allowedForward = False
+    elif mouse.ycor() <= -height and mouse.heading() == 270:
+        allowedForward = False
+    elif mouse.xcor() >= width and mouse.heading() == 0:
+        allowedForward = False
+    elif mouse.xcor() <= -width and mouse.heading() == 180:
+        allowedForward = False    
+
+    if allowedForward:
         mouse.forward(distance)
 
     s.update()
